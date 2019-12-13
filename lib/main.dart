71c6b9a325c75a5de4127523b86fb0163f6c78b2
Vicdd <victorrobcam@gmail.com>
2019-12-13 19:02:05 -0300

    Added some minor changes to layout to make it more responsive

--- lib/main.dart
+++ lib/main.dart
@@ -38,10 +38,9 @@ class _MyHomePageState extends State<MyHomePage> {
   int calculateAge(DateTime date) {
     DateTime now = DateTime.now();
     int age = now.year - date.year;
-    
+
     if (now.month < date.month) age--;
-    if (now.month == date.month) 
-      if (now.day < date.day) age--;
+    if (now.month == date.month) if (now.day < date.day) age--;
 
     return age;
   }
@@ -64,16 +63,19 @@ class _MyHomePageState extends State<MyHomePage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
-              height: MediaQuery.of(context).size.height/2,
+              height: MediaQuery.of(context).size.height / 2,
               decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage('assets/images/img1.png'),
-                  fit: BoxFit.fill
-                ),
+                    image: AssetImage('assets/images/img1.png'),
+                    fit: BoxFit.fill),
               ),
             ),
-            Container (
-              padding: EdgeInsets.only(top: 15.0, left: 30.0, right: 30.0), 
+            Container(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.02,
+                left: MediaQuery.of(context).size.width * 0.1,
+                right: MediaQuery.of(context).size.width * 0.1,
+              ),
               child: FormBuilder(
                 key: _fbKey,
                 autovalidate: false,
@@ -81,15 +83,19 @@ class _MyHomePageState extends State<MyHomePage> {
                   children: <Widget>[
                     Container(
                       decoration: BoxDecoration(
-                        border: Border(bottom: BorderSide(color: Color(0xFFE8E8E8), width: 5.0))
+                        border: Border(
+                          bottom: BorderSide(
+                            color: Color(0xFFE8E8E8),
+                            width: 5.0,
+                          ),
+                        ),
                       ),
                       child: FormBuilderTextField(
                         attribute: "name",
                         style: TextStyle(
-                          fontSize: 22.0,
-                          fontFamily: 'OpenSans',
-                          color: Color(0xFF757575)     
-                        ),
+                            fontSize: 22.0,
+                            fontFamily: 'OpenSans',
+                            color: Color(0xFF757575)),
                         decoration: InputDecoration(
                           labelText: "Full Name",
                           labelStyle: TextStyle(fontSize: 18),
@@ -100,18 +106,19 @@ class _MyHomePageState extends State<MyHomePage> {
                         validators: [FormBuilderValidators.required()],
                       ),
                     ),
-                    SizedBox(height: 30.0),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.06),
                     Material(
-                      elevation: 10.0,
+                      elevation: 8.0,
                       shadowColor: Colors.grey,
                       child: FormBuilderDropdown(
                         attribute: "job",
-                        hint: Text('Select Job', style: TextStyle(color: Color(0xFF757575), fontSize: 22)),
+                        hint: Text('Select Job',
+                            style: TextStyle(
+                                color: Color(0xFF757575), fontSize: 22)),
                         decoration: InputDecoration(
-                          filled: true, 
-                          border: InputBorder.none
-                        ),
-                        style: TextStyle(color: Color(0xFF757575), fontSize: 22),
+                            filled: true, border: InputBorder.none),
+                        style:
+                            TextStyle(color: Color(0xFF757575), fontSize: 22),
                         validators: [FormBuilderValidators.required()],
                         iconSize: 0,
                         items: [
@@ -119,65 +126,60 @@ class _MyHomePageState extends State<MyHomePage> {
                           'Adminstrador',
                           'Designer',
                           'Profissional de RH'
-                        ] .map((job) =>
-                            DropdownMenuItem(value: job, child: Text(job))).toList(),
+                        ]
+                            .map((job) =>
+                                DropdownMenuItem(value: job, child: Text(job)))
+                            .toList(),
                       ),
                     ),
                     SizedBox(height: 25.0),
                     Container(
                       decoration: BoxDecoration(
-                        border: Border(bottom: BorderSide(color: Color(0xFFE8E8E8), width: 5.0))
-                      ),
-                      child:
-                        FormBuilderDateTimePicker(
-                          attribute: "date",
-                          inputType: InputType.date,
-                          format: DateFormat("dd-MM-yyyy"),
-                          firstDate: DateTime(1900),
-                          lastDate: DateTime.now().add(Duration(hours: 1)),
-                          validators: [FormBuilderValidators.required()],
-                          style: TextStyle(
+                          border: Border(
+                              bottom: BorderSide(
+                                  color: Color(0xFFE8E8E8), width: 5.0))),
+                      child: FormBuilderDateTimePicker(
+                        attribute: "date",
+                        inputType: InputType.date,
+                        format: DateFormat("dd-MM-yyyy"),
+                        firstDate: DateTime(1900),
+                        lastDate: DateTime.now().add(Duration(hours: 1)),
+                        validators: [FormBuilderValidators.required()],
+                        style: TextStyle(
                             fontSize: 22.0,
                             fontFamily: 'OpenSans',
-                            color: Color(0xFF757575)             
-                          ),
-                          decoration: InputDecoration(
-                            labelText: "Date of birth",
-                            labelStyle: TextStyle(fontSize: 18),
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.symmetric(vertical: 5.0),
-                          ),
+                            color: Color(0xFF757575)),
+                        decoration: InputDecoration(
+                          labelText: "Date of birth",
+                          labelStyle: TextStyle(fontSize: 18),
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.symmetric(vertical: 5.0),
                         ),
+                      ),
                     ),
                     SizedBox(height: 42.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: <Widget>[
-                        Icon(
-                          Icons.arrow_left,
-                          size: 32,
-                          color: Color(0xFF0D2945),
-                        ),
-                        SizedBox(width: 30.0),
-                        Icon(
-                          Icons.fiber_manual_record,
-                          size: 12,
-                          color: Color(0xFFA6003E),
-                        ),
-                        SizedBox(width: 20.0),
-                        Image.asset(
-                          'assets/images/circle1.png',
-                          width:10, height: 10,
-                          fit: BoxFit.contain
-                        ),
-                        MaterialButton(
-                          onPressed: () {
-                            if (_fbKey.currentState.saveAndValidate()) {
-                              _sendDataToSecondScreen(context);
-                            }
-                          },
-                          child: Container( 
+                    Container(
+                      padding: EdgeInsets.only(
+                          left: MediaQuery.of(context).size.width * 0.2),
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.arrow_left,
+                            size: 28,
+                            color: Color(0xFF0D2945),
+                          ),
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.08),
+                          Icon(
+                            Icons.fiber_manual_record,
+                            size: 12,
+                            color: Color(0xFFA6003E),
+                          ),
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.04),
+                          Container(
+                            width: 9,
+                            height: 9,
                             decoration: BoxDecoration(
                               shape: BoxShape.circle,
                               gradient: LinearGradient(
@@ -189,11 +191,40 @@ class _MyHomePageState extends State<MyHomePage> {
                                 end: Alignment.bottomCenter,
                               ),
                             ),
-                            child: Icon(Icons.arrow_right, size: 20, color: Colors.white)
                           ),
-                        ),
-                      ],
-                    ),
+                          SizedBox(
+                              width: MediaQuery.of(context).size.width * 0.06),
+                          RawMaterialButton(
+                              constraints: BoxConstraints(
+                                maxWidth:
+                                    MediaQuery.of(context).size.width * 0.08,
+                                maxHeight:
+                                    MediaQuery.of(context).size.width * 0.08,
+                              ),
+                              onPressed: () {
+                                if (_fbKey.currentState.saveAndValidate()) {
+                                  _sendDataToSecondScreen(context);
+                                }
+                              },
+                              child: Container(
+                                  width: double.infinity, //match_parent
+                                  height: double.infinity,
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    gradient: LinearGradient(
+                                      colors: <Color>[
+                                        Color(0xFFA074E7),
+                                        Color(0xFFE6ABC1),
+                                      ],
+                                      begin: Alignment.topCenter,
+                                      end: Alignment.bottomCenter,
+                                    ),
+                                  ),
+                                  child: Icon(Icons.arrow_right,
+                                      color: Colors.white))),
+                        ],
+                      ),
+                    )
                   ],
                 ),
               ),
@@ -218,16 +249,19 @@ class SecondScreen extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
-            height: MediaQuery.of(context).size.height/2,
+            height: MediaQuery.of(context).size.height / 2,
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: AssetImage('assets/images/img2.png'),
-                fit: BoxFit.fill
-              ),
+                  image: AssetImage('assets/images/img2.png'),
+                  fit: BoxFit.fill),
             ),
           ),
           Container(
-            padding: EdgeInsets.only(top: 20.0, left: 30.0, right: 30.0),
+            padding: EdgeInsets.only(
+              top: MediaQuery.of(context).size.height * 0.03,
+              left: MediaQuery.of(context).size.width * 0.1,
+              right: MediaQuery.of(context).size.width * 0.1,
+            ),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -240,7 +274,7 @@ class SecondScreen extends StatelessWidget {
                     fontSize: 32,
                   ),
                 ),
-                SizedBox(height: 20.0),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.02),
                 Text(
                   data.personJob,
                   style: TextStyle(
@@ -248,7 +282,7 @@ class SecondScreen extends StatelessWidget {
                     fontSize: 32,
                   ),
                 ),
-                SizedBox(height: 20.0),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.02),
                 Text(
                   data.personAge.toString() + " anos",
                   style: TextStyle(
@@ -256,43 +290,40 @@ class SecondScreen extends StatelessWidget {
                     fontSize: 32,
                   ),
                 ),
-                SizedBox(height: 90.0),
+                SizedBox(height: MediaQuery.of(context).size.height * 0.165),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
                     IconButton(
                       icon: Icon(Icons.arrow_left),
-                      iconSize: 32,
                       color: Color(0xFFD4E1F3),
+                      iconSize: 28,
                       onPressed: () {
-                        Navigator.pop(context,
-                            CupertinoPageRoute(builder: (context) => MyHomePage()));
+                        Navigator.pop(
+                            context,
+                            CupertinoPageRoute(
+                                builder: (context) => MyHomePage()));
                       },
                     ),
-                    SizedBox(width: 30.0),
+                    SizedBox(width: MediaQuery.of(context).size.width * 0.08),
                     Icon(
                       Icons.fiber_manual_record,
                       size: 12,
                       color: Color(0xFF0D2945),
                     ),
-                    SizedBox(width: 20.0),
+                    SizedBox(width: MediaQuery.of(context).size.width * 0.04),
                     Icon(
                       Icons.fiber_manual_record,
                       size: 12,
                       color: Color(0xFFD4E1F3),
                     ),
-                    SizedBox(width: 40.0),
-                    Container( 
+                    SizedBox(width: MediaQuery.of(context).size.width * 0.1),
+                    Container(
                       decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Color(0xFFD4E1F3)
-                      ),
-                      child: Icon(
-                        Icons.arrow_right,
-                        size: 20,
-                        color: Color(0xFF0D2945)
-                      ),
+                          shape: BoxShape.circle, color: Color(0xFFD4E1F3)),
+                      child: Icon(Icons.arrow_right,
+                          size: 28, color: Color(0xFF0D2945)),
                     ),
                   ],
                 ),
